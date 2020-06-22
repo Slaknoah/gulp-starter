10d1c4e746440894ebc3f7da4b7cf659e7ccd1b4
slaknoah <slaknoah@gmail.com>
2020-06-22 16:52:33 +0600

    remove jquery dependency on sprite upload

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,22 @@
-let man = 'person';
+function loadSprites() {
+    var xmlFile="assets/svg/sprite.svg"
+    var loadXML = new XMLHttpRequest;
+    loadXML.onload = callback;
+    loadXML.open("GET", xmlFile, true);
+    loadXML.send();
+    function callback()
+    {
+        //---responseText---
+        var xmlString=loadXML.responseText
+        //---DOMParser---
+        var parser = new DOMParser();
+        var mySpritesDoc=parser.parseFromString(xmlString,"text/xml").documentElement ;
+        var addSprites=mySpritesDoc.childNodes
+        for(var k=0;k<addSprites.length;k++)
+        {
+           var sprite=addSprites.item(k).cloneNode(true)
+           document.getElementById("svg-sprite").appendChild(sprite)
+        }
+    }
+}
+loadSprites();
